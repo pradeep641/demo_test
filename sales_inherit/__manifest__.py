350357a80872a607016cd6c8ebe81c131f4babdb
pradeep641 <pradeepnirgude5@gmail.com>
2024-11-21 12:49:18 +0530

    Update __manifest__.py

--- sales_inherit/__manifest__.py
+++ sales_inherit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Add Custom Field in Sales',
-    'version': '1.0.0',
+    'version': '1.0',
     'summary': 'Adds a custom field to the Sales Order form after the date_order field.',
     'description': 'This module adds a custom field after the date_order field in the Sales Order form.',
     'author': 'Your Name',
